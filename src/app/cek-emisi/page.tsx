07c635ce70082ff5fb7efcdc20b46c4fda0f5b17
Almagribii <brucadalm@gmail.com>
2025-12-29 21:08:38 +0700

    fixed feateures display

--- src/app/cek-emisi/page.tsx
+++ src/app/cek-emisi/page.tsx
@@ -10,6 +10,7 @@ import solar from '../../../public/image/emisi/solar.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import Swal from 'sweetalert2';
+import { ButtonBack } from '@/components/ui/button-back';
 
 const transportasiData = [
     {
@@ -154,45 +155,40 @@ export default function Page() {
 
   return (
     <div className="" data-aos="fade-up" data-aos-duration="900">
-        <div className="w-full flex bg-white justify-between h-[11vh] lg:h-[14vh] items-center lg:px-20 px-6 fixed cursor-pointer z-30">
-          <Link href="/" className="flex justify-center gap-1 cursor-pointer items-center">
-            <i className="bx bx-chevron-left text-[24px] lg:text-[36px]" />
-            <h1 className="font-poppins text-[18px] font-[600] lg:text-[20px]">Kembali</h1>
-          </Link>
-        </div>
+      <ButtonBack/>
         <div className='flex flex-col justify-center items-center font-poppins min-h-screen pb-20 lg:pt-30 pt-24 lg:px-40 px-6 container mx-auto'>
-          <h1 className='lg:text-[34px] text-[20px] lg:text-start text-center font-[600] text-[#00AD03]'>Ayo Hitung Emisi Karbon Kamu Sekarang!</h1>
-          <div className="bg-[linear-gradient(150deg,_#58C229_30%,_#C7DF67_100%)] w-full min-h-[70vh] rounded-lg mt-10 text-white py-12">
+          <h1 className='lg:text-[34px] text-[20px] lg:text-start text-center font-semibold text-[#00AD03]'>Ayo Hitung Emisi Karbon Kamu Sekarang!</h1>
+          <div className="bg-[linear-gradient(150deg,#58C229_30%,#C7DF67_100%)] w-full min-h-[70vh] rounded-lg mt-10 text-white py-12">
             <div className="flex flex-col justify-center items-center gap-8">
-                <h1 className='lg:text-[30px] text-[20px] font-[600]'>Jenis Transportasi</h1>
+                <h1 className='lg:text-[30px] text-[20px] font-semibold'>Jenis Transportasi</h1>
                 <div className="flex justify-center flex-wrap items-center lg:gap-10 gap-4">
                     {transportasiData.map((item) => (
-                        <div onClick={() => handleChooseTransport(item.id)} className={`bg-white lg:w-auto w-[80px] lg:px-6 px-2 py-8 rounded-lg flex flex-col justify-center items-center gap-6 duration-400 hover:scale-95 cursor-pointer hover:opacity-80 ${checkedTransport == item.id ? 'opacity-80 scale-95' : ''}`} key={item.id}>
-                            <Image src={item.image} width={160} height={160} className="lg:w-[160px] w-[50px] h-auto" alt='image' />
-                            <p className='text-[#00AD03] font-[600] lg:text-[24px] text-[14px]'>{item.name}</p>
+                        <div onClick={() => handleChooseTransport(item.id)} className={`bg-white lg:w-auto w-20 px-6 py-8 rounded-lg flex flex-col justify-center items-center gap-6 duration-400 hover:scale-95 cursor-pointer hover:opacity-80 ${checkedTransport == item.id ? 'opacity-80 scale-95' : ''}`} key={item.id}>
+                            <Image src={item.image} width={160} height={160} className="lg:w-40 w-12.5 h-auto" alt='image' />
+                            <p className='text-[#00AD03] font-semibold lg:text-[24px] text-[14px]'>{item.name}</p>
                         </div>
                     ))}
                 </div>
             </div>
             <div className={`flex flex-col justify-center items-center gap-8 pt-12 ${filteredEmisiData.length > 0 ? 'flex' : 'show-none'}`}>
-                <h1 className='lg:text-[30px] text-[20px] font-[600]'>Jenis Bahan Bakar</h1>
+                <h1 className='lg:text-[30px] text-[20px] font-semibold'>Jenis Bahan Bakar</h1>
                 <div className="flex justify-center items-center lg:gap-10 gap-4">
                     {filteredEmisiData[0]?.emisi.map((item: any) => (
-                        <div onClick={() => handleEmisi(item.emisi, item.id)} className={`bg-white lg:w-auto w-[80px] lg:px-6 px-2 py-8 rounded-lg flex flex-col justify-center items-center gap-6 duration-400 hover:scale-95 cursor-pointer hover:opacity-80 ${checkedEmisi == item.id ? 'opacity-80 scale-95' : ''}`} key={item.id}>
-                            <Image src={item.image} width={160} height={160} className="lg:w-[160px] w-[50px] h-auto" alt='image' />
-                            <p className='text-[#00AD03] font-[600] lg:text-[24px] text-[14px]'>{item.name}</p>
+                        <div onClick={() => handleEmisi(item.emisi, item.id)} className={`bg-white lg:w-auto w-20 lg:px-6 px-2 py-8 rounded-lg flex flex-col justify-center items-center gap-6 duration-400 hover:scale-95 cursor-pointer hover:opacity-80 ${checkedEmisi == item.id ? 'opacity-80 scale-95' : ''}`} key={item.id}>
+                            <Image src={item.image} width={160} height={160} className="lg:w-40 w-12.5 h-auto" alt='image' />
+                            <p className='text-[#00AD03] font-semibold lg:text-[24px] text-[14px]'>{item.name}</p>
                         </div>
                     ))}
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center gap-4 pt-12">
-                <h1 className='lg:text-[26px] text-[18px] font-[600]'>Jarak Tempuh (Km)</h1>
-                <input value={distance} onChange={(e) => setDistance(e.target.value)} type="number" className='lg:text-[16px] lg:w-[300px] w-[200px] text-[14px] border-4 border-white rounded-xl px-3 outline-none py-2' />
+                <h1 className='lg:text-[26px] text-[18px] font-semibold'>Jarak Tempuh (Km)</h1>
+                <input value={distance} onChange={(e) => setDistance(e.target.value)} type="number" className='lg:text-[16px] lg:w-75 w-50 text-[14px] border-4 border-white rounded-xl px-3 outline-none py-2' />
             </div>
           </div>
-          <button onClick={handleTotalEmisi} className='bg-[#548D2E] text-white outline-none lg:text-[24px] text-[16px] font-[500] rounded-lg px-16 py-2 duration-200 border-2 border-[#548D2E] hover:bg-transparent hover:text-[#548D2E] cursor-pointer mt-12'>Hitung</button>
+          <button onClick={handleTotalEmisi} className='bg-[#548D2E] text-white outline-none lg:text-[24px] text-[16px] font-medium rounded-lg px-16 py-2 duration-200 border-2 border-[#548D2E] hover:bg-transparent hover:text-[#548D2E] cursor-pointer mt-12'>Hitung</button>
           <div className="flex flex-col gap-4 mt-12 w-full">
-            <h1 className='text-[#00AD03] font-[500] lg:text-[28px] text-[18px]'>Total</h1>
+            <h1 className='text-[#00AD03] font-medium lg:text-[28px] text-[18px]'>Total</h1>
             <input type="text" value={`${total} kg CO₂e`} disabled className='lg:text-[16px] text-[14px] border-4 border-[#00AD03] rounded-xl px-3 outline-none py-3' />
           </div>
         </div>
--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -12,6 +12,7 @@ import send from '../../../public/image/chatbot/send.svg';
 import mic from '../../../public/image/chatbot/mic.png';
 import load from '../../../public/image/chatbot/answer.svg';
 import logo from '../../../public/logo/chatbot-2.png';
+import { ButtonBack } from '@/components/ui/button-back';
 
 interface Message {
   role: 'user' | 'bot';
@@ -147,47 +148,26 @@ export default function GreenlyBot() {
 
   return (
     <div>
-      <div className={`absolute w-full h-screen font-poppins bg-[#00000024] z-50 flex-col justify-center items-center ${showMic ? 'flex' : 'hidden'}`}>
-        <div className="bg-white rounded-lg p-6 lg:w-[340px] w-[280px] lg:h-[340px] h-[280px] flex flex-col justify-center items-center gap-8">
-          <Image src={logo.src} className='bounce-show lg:w-[100px] w-[60px]' width={120} height={120} alt='image'/>
-          <p className='font-[500] lg:text-[20px] text-[16px]'>Mendengarkan...</p>
-          <div className="flex gap-1 items-end h-5">
-            <span className="w-1 bg-green-500 animate-wave1 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave1 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave2 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave3 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave2 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave1 rounded-sm"></span>
-            <span className="w-1 bg-green-500 animate-wave1 rounded-sm"></span>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full flex justify-between h-[11vh] lg:h-[14vh] items-center lg:px-20 px-6 fixed cursor-pointer z-30">
-        <Link href="/" className="flex justify-center gap-1 cursor-pointer items-center">
-          <i className="bx bx-chevron-left text-[24px] lg:text-[36px]" />
-          <h1 className="font-poppins font-[600] text-[18px] lg:text-[20px]">Kembali</h1>
-        </Link>
-      </div>
-
-      <div className="w-full flex-col flex fixed font-poppins justify-between h-[100vh] pt-24" data-aos="fade-up" data-aos-duration="800">
-        <Image src={glow} alt="Glow" width={900} height={600} className="absolute -z-20 w-[1200px] h-auto -bottom-60 left-1/2 -translate-x-1/2" />
+     
+      <ButtonBack/>
+      <div className="w-full flex-col flex fixed font-poppins justify-between h-screen pt-24" data-aos="fade-up" data-aos-duration="800">
+        <Image src={glow} alt="Glow" width={900} height={600} className="absolute -z-20 w-300 h-auto -bottom-60 left-1/2 -translate-x-1/2" />
 
         {showHeader && (
-          <div className="flex flex-col justify-center lg:mt-0 mt-12 items-center gap-8">
-            <Image src={ask} alt="Ask" width={35} height={35} className="lg:w-[35px] w-[25px] h-auto" />
-            <h1 className="font-[600] text-[16px] lg:text-[24px] text-center">Tanyakan apa saja kepada AI kami</h1>
+          <div className="flex flex-col justify-center lg:mt-0 mt-12 items-center gap-8 ">
+            <Image src={ask} alt="Ask" width={35} height={35} className="lg:w-8.75 w-6.25 h-auto" />
+            <h1 className="font-semibold text-[16px] lg:text-[24px] text-center ">Tanyakan apa saja kepada AI kami</h1>
           </div>
         )}
 
-        <div className="flex flex-col mx-auto w-[320px] lg:w-[900px] justify-between h-[calc(100vh-100px)] pb-14">
+        <div className="flex flex-col mx-auto w-[320px] lg:w-225 justify-between h-[calc(100vh-100px)] pb-14">
           <div className={`flex flex-col overflow-y-auto px-2 lg:px-6 py-4 space-y-4 ${messages.length > 0 ? 'h-full' : 'lg:h-[26vh] h-[16vh]'}`}>
             {messages.map((msg, index) => (
               <div key={index} className="flex flex-col gap-1 lg:gap-2">
-                <div className={`w-full text-[18px] text-[#56637E] flex ${msg.role === 'user' ? 'justify-end' : 'justify-start'}`}>
-                  <p className="font-[500]">{msg.role === 'user' ? 'Me' : 'Greenly Bot'}</p>
+                <div className={`w-full text-[18px] text-foreground flex ${msg.role === 'user' ? 'justify-end' : 'justify-start'}`}>
+                  <p className="font-medium">{msg.role === 'user' ? 'Me' : 'Greenly Bot'}</p>
                 </div>
-                <div className={`p-4 rounded-lg max-w-[95%] lg:max-w-[70%] shadow-md ${msg.role === 'user' ? 'bg-white text-gray-800 ml-auto' : 'bg-white text-gray-800 mr-auto'}`}>
+                <div className={`p-4 rounded-lg max-w-[95%] lg:max-w-[70%] shadow-md ${msg.role === 'user' ? 'bg-card text-foreground ml-auto' : 'bg-card text-foreground mr-auto'}`}>
                   <div className="text-[10px] lg:text-[14px] text-justify">
                     <ReactMarkdown>{msg.content}</ReactMarkdown>
                   </div>
@@ -205,7 +185,7 @@ export default function GreenlyBot() {
 
             {typingContent && (
               <div className="flex justify-start">
-                <div className="bg-white p-4 rounded-lg max-w-[95%] lg:max-w-[70%] shadow-md text-[10px] lg:text-[14px] text-justify text-gray-800">
+                <div className="bg-card p-4 rounded-lg max-w-[95%] lg:max-w-[70%] shadow-md text-[10px] lg:text-[14px] text-justify text-foreground">
                   <ReactMarkdown>{typingContent}</ReactMarkdown>
                 </div>
               </div>
@@ -215,10 +195,10 @@ export default function GreenlyBot() {
 
           {showSuggestions && (
             <div className="mb-0">
-              <h1 className="text-[#56637E] text-[14px] lg:text-[16px] mb-2 font-[500]">Saran tentang apa yang harus ditanyakan kepada AI kami</h1>
+              <h1 className="text-foreground text-[14px] lg:text-[16px] mb-2 font-medium">Saran tentang apa yang harus ditanyakan kepada AI kami</h1>
               <div className="flex flex-wrap gap-3 my-4">
                 {['Halo siapa kamu?', 'Bagaimana menjaga lingkungan?', 'Cara mendaur ulang sampah?'].map((question, idx) => (
-                  <button key={idx} onClick={() => handleAsk(question)} className="bg-white px-6 py-3 rounded-full text-[10px] lg:text-[12px] shadow-sm">
+                  <button key={idx} onClick={() => handleAsk(question)} className="bg-card px-6 py-3 rounded-full text-[10px] lg:text-[12px] shadow-sm">
                     {question}
                   </button>
                 ))}
@@ -226,7 +206,7 @@ export default function GreenlyBot() {
             </div>
           )}
 
-          <div className="flex items-center w-full bg-white h-[80px] px-4 lg:px-6 rounded-xl shadow-lg">
+          <div className="flex items-center w-full bg-card h-20 px-4 lg:px-6 rounded-xl shadow-lg">
             <input
               type="text"
               placeholder="Tanyakan apa pun tentang kondisi lingkungan Kamu sekarang"
@@ -238,8 +218,8 @@ export default function GreenlyBot() {
               onKeyDown={handleKeyDown}
               className="w-full outline-none text-[12px] lg:text-[14px] pr-4"
             />
-            <Image src={mic} alt="Mic" width={25} height={25} onClick={handleShowMic} className="cursor-pointer lg:w-[30px] w-[25px] mr-4" />
-            <Image src={send} alt="Send" width={25} height={25} onClick={handleSubmit} className="cursor-pointer lg:w-[25px] w-[20px]" />
+            <Image src={mic} alt="Mic" width={25} height={25} onClick={handleShowMic} className="cursor-pointer lg:w-7.5 w-6.25 mr-4" />
+            <Image src={send} alt="Send" width={25} height={25} onClick={handleSubmit} className="cursor-pointer lg:w-6.25 w-5" />
           </div>
         </div>
       </div>
