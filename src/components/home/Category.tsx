4984d2104bfcfc54c90999e6a96a450b6a20b479
Almagribii <brucadalm@gmail.com>
2026-01-06 01:39:46 +0700

    fixed category (change nested button to LINK)

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,16 +1,16 @@
-"use client"
-import React from 'react'
-import Link from 'next/link'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import Image from 'next/image'
-import tree from '../../../public/image/home/tree.webp'
-import water from '../../../public/image/home/water.webp'
-import air from '../../../public/image/home/air.webp'
-import dirt from '../../../public/image/home/dirt.webp'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Image from "next/image";
+import tree from "../../../public/image/home/tree.webp";
+import water from "../../../public/image/home/water.webp";
+import air from "../../../public/image/home/air.webp";
+import dirt from "../../../public/image/home/dirt.webp";
 
 const CategoryData = [
   {
@@ -45,16 +45,22 @@ const CategoryData = [
     link: "/udara",
     width: 200,
   },
-]
+];
 
 export default function Category() {
   return (
-    <div className='w-full container mx-auto px-4 font-poppins pb-26' data-aos="fade-up" data-aos-duration="800">
-      <h1 className='text-[#00AD03] lg:text-[36px] text-[28px] lg:w-full w-[300px] mx-auto font-[600] text-center mb-14'>Pilih Kategori Lingkungan!</h1>
-      
+    <div
+      className="w-full container mx-auto px-4 font-poppins pb-26"
+      data-aos="fade-up"
+      data-aos-duration="800"
+    >
+      <h1 className="text-[#00AD03] lg:text-[36px] text-[28px] lg:w-full w-75 mx-auto font-semibold text-center mb-14">
+        Pilih Kategori Lingkungan!
+      </h1>
+
       <Swiper
         modules={[Navigation, Pagination, EffectCoverflow]}
-        effect='coverflow'
+        effect="coverflow"
         centeredSlides={true}
         grabCursor={true}
         slidesPerView={3}
@@ -68,35 +74,48 @@ export default function Category() {
           slideShadows: false,
         }}
         breakpoints={{
-        0: {
-          slidesPerView: 1.1,
-          centeredSlides: true,
-        },
-        640: {
-          slidesPerView: 1.5,
-          centeredSlides: true,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }}
+          0: {
+            slidesPerView: 1.1,
+            centeredSlides: true,
+          },
+          640: {
+            slidesPerView: 1.5,
+            centeredSlides: true,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
       >
         {CategoryData.map((item) => (
           <SwiperSlide key={item.id}>
-            <div className="bg-[linear-gradient(150deg,_#58C229_30%,_#C7DF67_100%)] lg:w-[360px] w-full min-h-[460px] rounded-lg px-6 py-10 flex flex-col items-start justify-center text-center shadow-md mx-auto">
-              <Image src={item.image} alt='image' width={item.width} height={160} className="mb-4 mx-auto"/>
-              <h1 className="text-white text-[28px] font-[600]">{item.title}</h1>
-              <p className="text-white text-sm mt-2 text-[12px] text-justify">{item.desc}</p>
-              <Link href={item.link} className='w-full mt-8 cursor-pointer'>
-                <button className='w-full h-[40px] rounded-md text-[#AEA46B] bg-white'>Pilih</button>
-              </Link>
-            </div>
+            <Link
+              href={item.link}
+              className="block bg-[linear-gradient(150deg,#58C229_30%,#C7DF67_100%)] lg:w-90 w-full min-h-115 rounded-lg px-6 py-10 text-center shadow-md mx-auto hover:shadow-lg transition-shadow"
+            >
+              <Image
+                src={item.image}
+                alt="image"
+                width={item.width}
+                height={160}
+                className="mb-4 mx-auto"
+              />
+              <h1 className="text-white text-[28px] font-semibold">
+                {item.title}
+              </h1>
+              <p className="text-white text-sm mt-2 text-[12px] text-justify">
+                {item.desc}
+              </p>
+              <span className="block w-full h-10 mt-8 rounded-md text-[#AEA46B] bg-white leading-10 font-semibold">
+                Pilih
+              </span>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
-  )
+  );
 }
