2bc0de4b4cd16cb67c4872c2e2816002ca9cc675
mcqeems <mcqeemsofficial@gmail.com>
2026-01-06 02:39:13 +0700

    add scan metadata

--- src/app/(fitur)/scan/page.tsx
+++ src/app/(fitur)/scan/page.tsx
@@ -1,5 +1,11 @@
 import TrashScanner from './components/TrashScanner';
 
+export const metadata = {
+  title: 'Scan - Nazzava',
+  description:
+    'Periksa dan analisis sampahmu menggunakan Nazzava Trash Analyzer, temukan jenis sampah, rekomendasi dan deskripsi dari sampahmu!',
+};
+
 export default function Page() {
   return <TrashScanner />;
 }
